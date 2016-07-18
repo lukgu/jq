34a90f4f6d2d93b32caf87fc9096c2aedb3b7af3
lukgu <szukasz.gutowski@gmail.com>
2016-07-18 14:40:24 +0200

    jqery1+1

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $("span:odd").css('color','red');
 
 var paragraphs = $('p');
 paragraphs.each(function(index, element) {
-var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+var button = '<button class="btn" data-tmp="' + (index + 1) + '">Click me</button>'
 $(element).append(button)
 });
 
